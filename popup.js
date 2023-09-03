e99cef71aaccaef485b4534daa91381a7e79868b
SiNteTiKo <SuperAtic@users.noreply.github.com>
2023-09-03 08:31:29 +0700

    FireFox test

--- popup.js
+++ popup.js
@@ -1,7 +1,4 @@
 // popup.js
-const walletIframe = document.getElementById('walletIframe');
-
-// Load the appropriate URL based on the app version setting.
 chrome.storage.sync.get(['appVersion'], function (result) {
   const appVersion = result.appVersion || 'production'; // Default to production if setting not found.
   let walletUrl = 'https://wallet.nutstash.app';
@@ -11,4 +8,14 @@ chrome.storage.sync.get(['appVersion'], function (result) {
   }
 
   walletIframe.src = walletUrl;
+
+  // Check if the current page supports ecash payments (modify this logic accordingly).
+  const supportsEcashPayments = document.querySelector('meta[name="ecash-payment"][content="true"]') !== null;
+
+  // Change the browser action icon color based on support for ecash payments.
+  if (supportsEcashPayments) {
+    chrome.browserAction.setIcon({ path: { "48": "images/green_icon48.png" } });
+  } else {
+    chrome.browserAction.setIcon({ path: { "48": "images/icon48.png" } });
+  }
 });
